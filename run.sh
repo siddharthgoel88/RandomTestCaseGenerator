19eabb0c4d84a468e701174cbeec3e1f40a7522a
Siddharth Goel <siddharth98391@gmail.com>
2014-07-17 18:30:41 +0530

    Fixed shell script bug
    Java file compilation step was missing. So included that step in the
    shell script.
    
    Signed-off-by: Siddharth Goel <siddharth98391@gmail.com>

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 if [ ! -d "bin" ]; then
 	mkdir bin
 fi
+javac -cp lib/stdlib-package.jar:. -d bin/  src/com/rgen/RandomTestGenerator.java
 java -cp bin/:lib/stdlib-package.jar:. com.rgen.RandomTestGenerator
 
