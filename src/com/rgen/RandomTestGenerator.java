2320062849968732b3642782a64e8241925de4c0
Siddharth Goel <siddharth98391@gmail.com>
2014-07-17 14:56:25 +0530

    [Feature Addition] Added Pastebin upload feature
    User can now choose to upload the generated file to pastebin. Obviously,
    the user needs to enter his pastebin dev key once after which it will be
    locally stored on his machine.
    
    Signed-off-by: Siddharth Goel <siddharth98391@gmail.com>

--- src/com/rgen/RandomTestGenerator.java
+++ src/com/rgen/RandomTestGenerator.java
@@ -1,4 +1,12 @@
 package com.rgen;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+
 import edu.princeton.cs.introcs.*;
 
 public class RandomTestGenerator {
@@ -15,8 +23,73 @@ public class RandomTestGenerator {
 	public void init() {
 		this.takeInput();
 		this.makeFile();
+		this.generateLink();
 	}
 	
+	private void generateLink() {
+		String option;
+		StdOut.print("Do you want to upload this file to pastebin? (Y/N):");
+		option = StdIn.readString().trim();
+		if (option != "y" || option == "yes")
+			try {
+				generatePastebinLink();
+			} catch(Exception e) {
+				e.printStackTrace();
+			}
+	}
+
+	private void generatePastebinLink() throws Exception {
+		String url = "http://pastebin.com/api/api_post.php";
+		String pastebinDevKey = "";
+		URL obj = new URL(url);
+		HttpURLConnection conn = (HttpURLConnection) obj.openConnection();
+		File devKeyFile = new File("./.pastebin-devkey");
+
+		if(!devKeyFile.exists()) {
+			StdOut.println("Oh. I do not have your pastebin dev. "
+					+ "Login pastebin and visit http://pastebin.com/api "
+					+ "to get your dev key.I will try to remember it.\n"
+					+ "Enter your key:");
+			pastebinDevKey = StdIn.readString().trim();
+			Out file = new Out(".pastebin-devkey");
+			file.print(pastebinDevKey);
+			file.close();
+		} else {
+			In file = new In(devKeyFile);
+			pastebinDevKey = file.readString().trim();
+			file.close();
+		}
+
+		String urlParameters = "api_option=paste&api_paste_private=0&"
+				+ "api_dev_key=" + pastebinDevKey + "&api_paste_name="
+				+ filename + "&api_paste_code=" + URLEncoder.encode(result, "UTF-8");
+
+		//Sending post
+		StdOut.println("Sending ...");
+		conn.setDoOutput(true);
+		DataOutputStream writer = new DataOutputStream(conn.getOutputStream());
+		writer.writeBytes(urlParameters);
+		writer.flush();
+		writer.close();
+		StdOut.println("Done sending!");
+
+		//Response
+		StdOut.println("Receiving response ...");
+		BufferedReader in = new BufferedReader(
+		        new InputStreamReader(conn.getInputStream()));
+		String inputLine;
+		StringBuffer response = new StringBuffer();
+
+		while ((inputLine = in.readLine()) != null) {
+			response.append(inputLine);
+		}
+		in.close();
+		StdOut.println("Done receiving !");
+
+		//print result
+		System.out.println(response.toString());
+	}
+
 	//Take test-case related input from the user.
 	private void takeInput() {
 		String option;
